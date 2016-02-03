24abfe2e0b1ffa4be8bae318b4a274c0a61730ff
sadielulu <suecarbajal@gmail.com>
2016-02-03 04:28:16 -0500

    Update Server.java

--- ClientServer/src/Server.java
+++ ClientServer/src/Server.java
@@ -4,24 +4,20 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(12345);
-		//Socket socket.bind(new InetSocketAddress("128.6.36.180", 12345));
-		Socket socket=server.accept();
-
+	//Socket socket.bind(new InetSocketAddress("128.6.36.180", 12345));
+	Socket socket=server.accept();
         long time = System.currentTimeMillis();
-		//read from client
-		InputStream is =socket.getInputStream();
+	//read from client
+	InputStream is =socket.getInputStream();
         InputStreamReader isr = new InputStreamReader(is);
         BufferedReader br = new BufferedReader(isr);
         String string= br.readLine();
-       long startTime= Long.parseLong(string, 10);  //CONVERT from STRING TO long START TIME
+       	long startTime= Long.parseLong(string, 10);  //CONVERT from STRING TO long START TIME
         long total = time - startTime;
-
-
-    		//write back to user
-    		PrintWriter out =new PrintWriter(socket.getOutputStream(), true);
-    		out.println(""+total);
-    		//InputStream nis = socket.getInputStream();
-    		//BufferedReader reader = new BufferedReader(new InputStreamReader(nis));
-        
+    	//write back to user
+    	PrintWriter out =new PrintWriter(socket.getOutputStream(), true);
+    	out.println(""+total);
+    	//InputStream nis = socket.getInputStream();
+    	//BufferedReader reader = new BufferedReader(new InputStreamReader(nis));
     }
 } 
